76c327869f55a75d98058ca875307f8752831d0c
poojapardeshi02 <poojapardeshi02@gmail.com>
2023-02-16 03:47:13 -0800

    update helloworld.js & Create 1_variableAssign.js file
    update helloworld.js & Create 1_variableAssign.js file

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+console.log("****Step 1****");
+var myFullName;
+myFullName="Pooja Pardeshi";
+console.log("First & Last Name:",myFullName);
+myFullName="Pooja Vikram Pardeshi";
+console.log("Full Name:",myFullName);
+
+console.log("**** Step 2****");
+var myLove="I love only JS";
+console.log("My Love:",myLove);
+console.log("**** Step 3****");
+var firstName="First Name: Pooja";
+console.log(firstName);
+var lastName="Last Name: Pardeshi";
+console.log(lastName);
+var age="Age:29";
+console.log(age);
+var address="Address: Gajanan Chowk,Phaltan";
+console.log(address);
+var pincode="Pincode: 415523";
+console.log(pincode);
+var gender="Gender:Female";
+console.log(gender);
+var isMarried="Is Married:Yes";
+console.log(isMarried);
+console.log("**** Step 4****");
+console.log(firstName,lastName,age,pincode,gender,isMarried)
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,10 @@
-console.log("Pooja Pardeshi ")
+console.log("Pooja Pardeshi ");
+console.log(31);
+console.log("Pune");
+
+var fullname="Pooja Pardeshi";
+console.log(fullname);
+console.log(fullname);
+
+var city;
+console.log(city);
