28ed01a279b1c986f6673b49563e2ff20ef99f50
poojapardeshi02 <poojapardeshi02@gmail.com>
2023-02-18 23:48:29 -0800

    updated 3 files
    updated 3 files

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = 31;
+var weight = 82.5;
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+
+console.log("typeOf variable age:", typeof age);
+
+
+
+console.log("typeOf variable isMarried:", typeof isMarried);
+
+var graduation;
+console.log("Value of  graduation variable is: ", graduation);
+console.log("typeOf graduation is: ",typeof graduation);
+
+var city=null;
+console.log("Value of  city variable is: ",city);
+console.log("typeOf city  is: ",typeof city);
