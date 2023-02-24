b593ff3b57c9332b48f54fd2f24d3f97cd0484b2
poojapardeshi02 <poojapardeshi02@gmail.com>
2023-02-24 03:45:33 -0800

    some changes in yesterday assignments
    some changes yesterday in assignments

--- 05_operatorAssig0A.js
+++ 05_operatorAssig0A.js
@@ -1,36 +1,38 @@
 console.log(`** Step 1 **`);
-function greaterNumber(num1,num2){
-     var result=num1 > num2 ? `Num 1 is greater value which is: ${num1}` : `Num 2 is greater value which is: ${num2}`;
-     console.log(result);
-}
-greaterNumber(10,-10);
-greaterNumber(800,899);
+function greaterNumber(num1, num2){
+    var result = num1>num2 ? num1 : num2;
+     console.log(`Greater number amongst ${num1}, ${num2} is: ${result} `);
+ }
+ greaterNumber(10, -10);
+ greaterNumber(800, 899);
+ 
+
 
 console.log(`** Step 2 **`);
-function isEvenOrOddNum(num){
-      var result1=((num % 2) == 0) ? `The value ${num} is even:true` : `The value ${num} is even:false`;
-      return result1;
-    }
-var result1=isEvenOrOddNum(29);
+function isEvenOrOddNum(num) {
+    var result1 = ((num % 2) == 0) ? `The value ${num} is even:true` : `The value ${num} is odd:false`;
+    return result1;
+}
+var result1 = isEvenOrOddNum(29);
 console.log(result1);
-var result1=isEvenOrOddNum(44);
+var result1 = isEvenOrOddNum(44);
 console.log(result1);
-var result1=isEvenOrOddNum(0);
+var result1 = isEvenOrOddNum(0);
 console.log(result1);
-var result1=isEvenOrOddNum(101);
+var result1 = isEvenOrOddNum(101);
 console.log(result1);
 
 console.log(`** Step 3 **`);
-function wordLength(string){
-    var wordLengthFind=string.length
-    var result2=(wordLengthFind % 2) == 0 ?  "Even" :  "odd";
+function wordLength(string) {
+    var wordLengthFind = string.length
+    var result2 = (wordLengthFind % 2) == 0 ? "Even" : "odd";
     return result2;
 }
-var result2=wordLength("Javascript");
-console.log( `The Word "JavaScript"  has ${result2} length `);
+var result2 = wordLength("Javascript");
+console.log(`The Word "JavaScript"  has ${result2} length `);
 
-var result2=wordLength("Developer");
-console.log( `The Word "Developer" has ${result2} length`);
+var result2 = wordLength("Developer");
+console.log(`The Word "Developer" has ${result2} length`);
 
-var result2=wordLength("Google");
-console.log( `The Word "Google" has ${result2} length`);
+var result2 = wordLength("Google");
+console.log(`The Word "Google" has ${result2} length`);
--- 05_operatorAssig0B.js
+++ 05_operatorAssig0B.js
@@ -1,20 +1,19 @@
 console.log(`** Step 1 **`);
-function maleMarriageEligibility(gender, age, boyName){
-    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are eligible for marriage` : ` Hey ${boyName} you are not eligible for marriage` ;
+function maleMarriageEligibility(gender, age, boyName) {
+    var result = (gender == "Male" && age >= 21) ? `Hey ${boyName} you are eligible for marriage` : `Hey ${boyName} you are not eligible for marriage`;
     return result;
- }
-  var result = maleMarriageEligibility("Male", 25, "Billgates");
-  console.log(result);
-  var result = maleMarriageEligibility("Male", 17, "Stew Jobs");
-  console.log(result);
+}
+var result = maleMarriageEligibility("Male", 25, "Billgates");
+console.log(result);
+var result = maleMarriageEligibility("Male", 17, "Stew Jobs");
+console.log(result);
 
-  console.log(`** Step 2 **`);
-  function femaleMarriageEligibility(gender, age, girlName){
-    var result1 = (gender=="Female" && age>=18) ? `Hey ${girlName} you are eligible for marriage` : ` Hey ${girlName} you are not eligible for marriage`;
+console.log(`** Step 2 **`);
+function femaleMarriageEligibility(gender, age, girlName) {
+    var result1 = (gender == "Female" && age >= 18) ? `Hey ${girlName} you are eligible for marriage` : `Hey ${girlName} you are not eligible for marriage`;
     return result1;
- }
-  var result1 = femaleMarriageEligibility("Female", 16, "Jenifer");
-  console.log(result1);
-  var result1 = femaleMarriageEligibility("Female", 27, "Malinda Gates");
-  console.log(result1);
-  
+}
+var result1 = femaleMarriageEligibility("Female", 16, "Jenifer");
+console.log(result1);
+var result1 = femaleMarriageEligibility("Female", 27, "Malinda Gates");
+console.log(result1);
