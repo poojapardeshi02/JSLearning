c961afdc5310122878fee9bc99fec9cfc9657ce7
poojapardeshi02 <poojapardeshi02@gmail.com>
2023-03-28 21:23:06 +0530

    Create files on DOM
    Create file on DOM

---
+++ DOM/script_3.js
@@ -0,0 +1,11 @@
+console.log("------h1 element store in mainheading------");
+const mainheading = document.getElementById("heading");
+console.log(mainheading.innerHTML);
+
+console.log("------second li element store in secondAdvantage------");
+const secondAdvantage = document.querySelector("#avai");
+console.log(secondAdvantage.innerHTML);
+
+console.log("------store div with id ------");
+const addDiv = document.getElementById("advantages");
+console.log(addDiv.innerHTML);
